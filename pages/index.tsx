bbcbb2caa2b9f66f4065d59349d92d9032778764
markfuturlix <81425037+markfuturlix@users.noreply.github.com>
2022-02-14 12:17:48 +0000

    syntax fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -99,10 +99,10 @@ export default function Home() {
           </div>
           <div className="flex-1 bg-[#b9b9e7]"></div>
         </div>
-        <a name="video">
+        <a id="video">
           <div className="prose-lg mx-auto max-w-4xl px-3 pt-12 leading-6 md:px-6">
             <div className="relative h-0 pb-[56.25%]">
-              <iframe className="absolute w-full h-full top-0 left-0" src="https://www.youtube.com/embed/lwD1NNR875A?cc_load_policy=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              <iframe className="absolute w-full h-full top-0 left-0" src="https://www.youtube.com/embed/lwD1NNR875A?cc_load_policy=1" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
             </div>
           </div>
         </a>
